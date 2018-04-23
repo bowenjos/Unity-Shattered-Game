c64a1713589b6680f8d8b74c90105b4212f6e867
Joshua Bowen <joshuadavidbowen@gmail.com>
2018-04-22 23:52:48 -0700

    The room William will first appear in, and some boxes

--- Assets/Scripts/Data/Scenes/DeadPerformance/DPMainData.cs
+++ Assets/Scripts/Data/Scenes/DeadPerformance/DPMainData.cs
@@ -9,6 +9,9 @@ public class DPMainData {
     //Porgression Data
     int progression = 0;
     bool williamTalked = false;
+    bool leverOne = false;
+    bool leverTwo = false;
+    bool leverThree = false;
 
     //Enemy Data to come
 
