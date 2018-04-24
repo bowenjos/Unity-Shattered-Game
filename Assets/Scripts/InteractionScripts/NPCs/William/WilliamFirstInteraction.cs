bd3f73b03691ea01f62a102a3c4479943ea595d1
Joshua Bowen <joshuadavidbowen@gmail.com>
2018-04-23 22:23:34 -0700

    New interaction trigger in the form of a trigger zone.

---
+++ Assets/Scripts/InteractionScripts/NPCs/William/WilliamFirstInteraction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WilliamFirstInteraction : TriggerInteraction {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override IEnumerator StartInteraction()
+    {
+        return base.StartInteraction();
+    }
+}
