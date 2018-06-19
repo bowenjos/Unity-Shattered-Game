c8a816b35673b10996359d2f6dd6d83eb0cb70be
Joshua Bowen <joshuadavidbowen@gmail.com>
2018-06-18 18:02:05 -0700

    A new friendly ghost and some more spike related things

--- Assets/Scripts/Objects/DP/ActivatedObject.cs
+++ Assets/Scripts/Objects/DP/ActivatedObject.cs
@@ -7,28 +7,49 @@ public class ActivatedObject : MonoBehaviour {
     public ButtonEffect button;
 
     public Sprite enabledSprite;
+    public Sprite middleSprite;
     public Sprite disabledSprite;
 
     SpriteRenderer sr;
     BoxCollider2D bx;
 
+    bool activatedHere;
+
 	// Use this for initialization
 	void Start () {
         sr = GetComponent<SpriteRenderer>();
         bx = GetComponent<BoxCollider2D>();
+        activatedHere = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(button.activated == true)
+		if(button.activated == true && activatedHere != button.activated)
         {
-            sr.sprite = enabledSprite;
-            bx.enabled = false;
+            StartCoroutine(Activate());
         }
-        else
+        else if(button.activated == false && activatedHere != button.activated)
         {
-            sr.sprite = disabledSprite;
-            bx.enabled = true;
+            StartCoroutine(Deactivate());
         }
 	}
+
+    IEnumerator Activate()
+    {
+        activatedHere = true;
+        sr.sprite = middleSprite;
+        yield return new WaitForSeconds(0.05f);
+        sr.sprite = enabledSprite;
+        bx.enabled = false;
+        activatedHere = true;
+    }
+
+    IEnumerator Deactivate()
+    {
+        activatedHere = false;
+        sr.sprite = middleSprite;
+        yield return new WaitForSeconds(0.05f);
+        sr.sprite = disabledSprite;
+        bx.enabled = true;
+    }
 }
