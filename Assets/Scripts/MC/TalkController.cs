0e77879a5fc5942e785dbb57e5cc8ca4eb942e80
Joshua Bowen <joshuadavidbowen@gmail.com>
2018-09-07 18:00:56 -0700

    Comments for StairsEffect and TalkController

--- Assets/Scripts/MC/TalkController.cs
+++ Assets/Scripts/MC/TalkController.cs
@@ -236,7 +236,6 @@ public class TalkController : MonoBehaviour {
     Purpose: Animates a given text string in the style of "Typewriter Text" where text is printed character by character
     Pre: A line of dialogue must be given
     Post: The text has finished printing character by character and the function returns
-    To-Do: Add option to skip and not wait for the dialogue to print
     ********************/
 
     protected IEnumerator AnimateText(string text)
@@ -246,34 +245,47 @@ public class TalkController : MonoBehaviour {
 
         Coroutine co = StartCoroutine(SkipText(text));
         yield return new WaitForEndOfFrame();
+        //For each character in the dialogue string
         for (i = 0; i < (text.Length); i++)
         {
-
+            //First, check to see if the character is the beginning (or beginning of the end) of a Markdown styliing
             switch (text[i])
             {
+                //If the character is < (the beginning of a markdown styling)
                 case '<':
+                    //If the text is not current marked
                     if (marked == false)
                     {
+                        //Begin markdown
                         i = ResolveMarkdown(i, text);
                     }
                     else {
+                        //End Markdown
                         i = FinishMarkdown(i, text);
                     }
                     break;
+                //If the character is not a markdown character
                 default:
+                    //If the text being written is currently styled with markdown
                     if (marked == true)
                     {
+                        //Append the markdown string to the end of the substring (EX: </color>)
                         editText.text = text.Substring(0, i) + markdown;
                     }
                     else
                     {
+                        //Act natural (print the current step of the typewritter text string
                         editText.text = text.Substring(0, i);
                     }
                     break;
             }
            
+            //If this character isn't the very first character
             if (i != 0)
             {
+                //Check the last printed character
+                //This switch case essentially servers to add slight time delays after periods and commas, or defaults to the normal time delay
+                //I might/will probably add future functionality for variable delays, but I also might just do that in a different function, or maybe just put this in it's own function Generally
                 switch (text[i - 1])
                 {
                     case ',':
@@ -294,16 +306,27 @@ public class TalkController : MonoBehaviour {
                 }
             }
         }
+        //Stop the coroutine that lets you skip past all the text if you don't want to watch it print
         StopCoroutine(co);
         editText.text = text;
     }
+
+    /********************
+    Function Name: ResolveMarkdown
+    Function Type: Int
+    Purpose: Allows the usage of Markdown style in the text and also typewriting
+    Pre: A line of dialogue must be given, and a position in the string must be given
+    Post: The end of the markdown string has been received, the string has been updated
+    ********************/
 
     protected int ResolveMarkdown(int i, string text)
     {
         int j = 0;
         int k = 0;
         int l = 0;
+        //Set the marked variable to true (for use in the main Animate Text function)
         marked = true;
+        //Find the end of the first set of markdown (EX: <color=blue>)
         for (j = i; j < text.Length; j++)
         {
             if (text[j] == '>')
@@ -311,28 +334,36 @@ public class TalkController : MonoBehaviour {
                 break;
             }
         }
-
+        //Find the beginning of the end of the markdown (EX: </color>) Save that substring into a string
         for (l = j+2; l < text.Length; l++) { 
             if (text[l] == '<') {
                 k = l;
-                Debug.Log(k);
-                Debug.Log(text[k]);
             }
             if (text[l] == '>')
             {
-                Debug.Log(l);
                 markdown = text.Substring(k, l - k + 1);
-                Debug.Log(markdown);
-                Debug.Log(l);
             }
         }
+        //Set the current string to whatever the substring was, but skipped forward to the end of the beginning of the markdown and with the end appended on
+        //It should look like this (EX: this part of the string isn't markdown but <i></i>)
+        //When the string continues on the rest of the string wille be within the markdown until the markdown ends
         editText.text = text.Substring(0, j + 1) + markdown;
+        //Return the value of the position inthe substring that is after the first set of markdown
         return j;
 
     }
 
+    /********************
+    Function Name: FinishMarkdown
+    Function Type: Int
+    Purpose: Concludes the usage of Markdown style in the text and also typewriting
+    Pre: A line of dialogue must be given
+    Post: traversing the markdown is completed
+    ********************/
+
     protected int FinishMarkdown(int i, string text)
     {
+        //Set marked to false, and skip forward the length of the markdown string
         marked = false;
         i = i + markdown.Length;
         editText.text = text.Substring(0, i);
--- Assets/Scripts/Objects/StairsEffect.cs
+++ Assets/Scripts/Objects/StairsEffect.cs
@@ -39,29 +39,41 @@ public class StairsEffect : MonoBehaviour {
 
     void OnTriggerStay2D(Collider2D col)
     {
+        //Double checks that the colliding object is the player
         if (col.gameObject.tag == "Player")
         {
+            //Checks to make sure that the pCon has been retrieved from OnTriggerEnter2d
             if (pCon != null)
             {
+                //If the player is walking and their direction is Right
                 if (pCon.walking && pCon.direction == 1)
                 {
+                    //If the direction of the staircase is going downright relative to the player
                     if (direction == 1)
                     {
+                        //Lower the player downwards
                         col.gameObject.transform.position += Vector3.down * intensity * Time.deltaTime;
                     }
+                    //If the direction of the staircase is going upright relative to the player
                     else if (direction == 3)
                     {
+                        //Raise the player upwards
                         col.gameObject.transform.position += Vector3.up * intensity * Time.deltaTime;
                     }
                 }
+                //If the player is walking and their direction is Left
                 else if (pCon.walking && pCon.direction == 3)
                 {
+                    //If the direction of the staircase is going upleft relative to the player
                     if (direction == 1)
                     {
+                        //Raise the player upwards
                         col.gameObject.transform.position += Vector3.up * intensity * Time.deltaTime;
                     }
+                    //If the direction of the staircase is going downleft relative to the player
                     else if (direction == 3)
                     {
+                        //Lower the player downwards
                         col.gameObject.transform.position += Vector3.down * intensity * Time.deltaTime;
                     }
                 }
@@ -71,6 +83,7 @@ public class StairsEffect : MonoBehaviour {
 
     void OnTriggerExit2D()
     {
+        //When the player leaves the staircase zone reset the staircase
         isColliding = false;
         pCon.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
         pCon = null;
