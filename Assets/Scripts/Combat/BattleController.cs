2222a7f8446b6c01023b90aefa393eb45644a1ae
Joshua Bowen <joshuadavidbowen@gmail.com>
2019-01-21 17:10:12 -0800

    Begin Revised Combat

---
+++ Assets/Scripts/Combat/BattleController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleController : MonoBehaviour {
+
+    public static BattleController BC;
+    public enum BattleState {Player, Enemy};
+    public BattleState currentState;
+
+    //Enemy Variables
+    public int enemyHealthMax;
+    public int enemyHealthCurrent;
+    public string enemyEmotion;
+    public int[,] enemyResistances;
+
+    //Battle Rules
+    bool fleeable;
+
+    //Previous Scene Data
+    string oldSceneName;
+    int playerLocationX;
+    int playerLocationY;
+    int playerLocationZ;
+
+    //Entities
+    EnemyCombatControl Enemy;
+    TalkControlDialogue tcd;
+
+    void Awake()
+    {
+        Enemy = GameObject.Find("Enemy").GetComponent<EnemyCombatControl>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		//Are you ready to Begin?
+        //StartCoroutine(tcd.Dialogue(Enemy.dialogueApproach);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
