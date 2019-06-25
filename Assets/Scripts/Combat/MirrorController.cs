81b1a2c5a1c7d8b4b949f10c21716fd673d038f7
Joshua Bowen <joshuadavidbowen@gmail.com>
2019-06-24 18:36:41 -0700

    Mirror cracks as it takes damage

--- Assets/Scripts/Combat/MirrorController.cs
+++ Assets/Scripts/Combat/MirrorController.cs
@@ -13,6 +13,12 @@ public class MirrorController : MonoBehaviour {
     protected ContactFilter2D contactFilter;
     protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
     protected Collider2D mirrorCollider;
+
+    public Sprite mirror1;
+    public Sprite mirror2;
+    public Sprite mirror3;
+    public Sprite mirror4;
+    public Sprite mirror5;
     
 
     // Use this for initialization
@@ -23,7 +29,28 @@ public class MirrorController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(GameControl.control.health <= 0)
+
+        if(GameControl.control.health >= GameControl.control.maxHealth * 0.8f)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = mirror1;
+        }
+        else if(GameControl.control.health >= GameControl.control.maxHealth * 0.6f)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = mirror2;
+        }
+        else if (GameControl.control.health >= GameControl.control.maxHealth * 0.4f)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = mirror3;
+        }
+        else if (GameControl.control.health >= GameControl.control.maxHealth * 0.2f)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = mirror4;
+        }
+        else if (GameControl.control.health >= 0f)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = mirror5;
+        }
+        if (GameControl.control.health <= 0)
         {
             //Play Shattered Effect
             StopAllCoroutines();
