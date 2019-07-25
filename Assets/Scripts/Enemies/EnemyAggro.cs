00370d5bda7d04d9323a35bed8556e81ae8f4e68
Joshua Bowen <joshuadavidbowen@gmail.com>
2019-07-24 20:02:29 -0700

    Improved Enemys
    Added Enemy Roaming + Aggro

---
+++ Assets/Scripts/Enemies/EnemyAggro.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAggro : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+    public EnemyWalk enemyWalk;
+    public Transform enemyTransform;
+    public Animator anim;
+
+    protected bool moved;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moved = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.name == "player(Clone)")
+        {
+            enemyWalk.aggro = true;
+            StartCoroutine(enemyWalk.Hop());
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D col)
+    {
+        
+        if (col.gameObject.name == "player(Clone)" && !enemyWalk.hopping && !moved)
+        {
+            anim.SetBool("walking", true);
+            StartCoroutine(Move(col));
+        }
+    }
+
+    private IEnumerator Move(Collider2D col)
+    {
+        moved = true;
+        if (col.transform.position.x > enemyTransform.position.x)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
+        enemyTransform.position = Vector3.MoveTowards(enemyTransform.position, col.transform.position, 3 * enemyWalk.speed);
+        yield return new WaitForSeconds(0.01f);
+        moved = false;
+    }
+
+    public void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name == "player(Clone)")
+        {
+            enemyWalk.aggro = false;
+            enemyWalk.moving = false;
+            anim.SetBool("walking", false);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyWalk.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyWalk : MonoBehaviour
+{
+    public float speed;
+    public float idleTime;
+
+    public SpriteRenderer sprite;
+    public Transform enemyTransform;
+    public BoxCollider2D range;
+    protected Bounds enemyRange;
+    public Animator anim;
+    public Rigidbody2D rb2d;
+
+    public bool moving;
+    public bool aggro;
+    public bool hopping;
+
+    protected Coroutine co;
+
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Encounter")
+        {
+            Destroy(this);
+        }
+ 
+        enemyRange = range.bounds;
+        hopping = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!moving && !aggro)
+        {
+            Vector3 destination = RandomPointInBounds(enemyRange);
+            Debug.Log("Destination: " + destination);
+            co = StartCoroutine(Move(destination));
+        }
+        if (aggro)
+        {
+            StopCoroutine(co);
+        }
+    }
+
+
+
+    protected IEnumerator Move(Vector3 destination)
+    {
+        moving = true;
+        yield return new WaitForSeconds(idleTime);
+        anim.SetBool("walking", true);
+        if (destination.x > enemyTransform.position.x)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
+        while (!(Vector3.Distance(enemyTransform.position, destination) < 0.01f))
+        {
+            enemyTransform.position = Vector3.MoveTowards(enemyTransform.position, destination, speed);
+            yield return new WaitForSeconds(.01f);
+        }
+        anim.SetBool("walking", false);
+        moving = false;
+
+    }
+
+    protected Vector3 RandomPointInBounds(Bounds bounds)
+    {
+        float randy = Random.Range(bounds.min.y, bounds.max.y);
+        return new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            randy,
+            randy
+            );
+    }
+
+    public IEnumerator Hop()
+    {
+        hopping = true;
+        float x = enemyTransform.position.x;
+        float y = enemyTransform.position.y;
+        enemyTransform.position = new Vector3(x, y + .01f, y);
+        yield return new WaitForSeconds(0.01f);
+        enemyTransform.position = new Vector3(x, y + .02f, y);
+        yield return new WaitForSeconds(0.01f);
+        enemyTransform.position = new Vector3(x, y + .03f, y);
+        yield return new WaitForSeconds(0.04f);
+        enemyTransform.position = new Vector3(x, y + .02f, y);
+        yield return new WaitForSeconds(0.01f);
+        enemyTransform.position = new Vector3(x, y + .01f, y);
+        yield return new WaitForSeconds(0.01f);
+        enemyTransform.position = new Vector3(x, y, y);
+        hopping = false;
+    }
+}
