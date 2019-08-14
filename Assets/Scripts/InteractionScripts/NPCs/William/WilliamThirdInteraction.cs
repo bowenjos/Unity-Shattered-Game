293a2d11d1161ba047122fb64b45e67934cb57f5
Joshua Bowen <joshuadavidbowen@gmail.com>
2019-08-13 17:37:46 -0700

    Will Third Interaction
    is done, with some secret bonus dialogue if you don't follow instruction

--- Assets/Scripts/InteractionScripts/NPCs/William/WilliamThirdInteraction.cs
+++ Assets/Scripts/InteractionScripts/NPCs/William/WilliamThirdInteraction.cs
@@ -17,12 +17,13 @@ public class WilliamThirdInteraction : CharacterInteraction
         talkCanvas = GameObject.Find("Talk UI(Clone)").GetComponent<TalkController>();
 
         dialogue = new string[1][];
-        dialogue[0] = new string[5];
+        dialogue[0] = new string[6];
         dialogue[0][0] = "Look... I. I really appreciate your help.";
         dialogue[0][1] = "I'm sorry again how I ran away when I first saw you.";
-        dialogue[0][2] = "I'm supposed to be playing tonight, but it's the first time I'll have played for an Audience.";
-        dialogue[0][3] = "Before this, Elise was the only person who has ever seen me play.";
-        dialogue[0][4] = "Now, lets go find those actors.";
+        dialogue[0][2] = "I'm supposed to be playing tonight,";
+        dialogue[0][3] = "but it's the first time I'll have played for an Audience.";
+        dialogue[0][4] = "Before this, Elise was the only person who has ever seen me play.";
+        dialogue[0][5] = "Now, lets go find those actors.";
     }
 
     // Update is called once per frame
